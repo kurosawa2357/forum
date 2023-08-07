5c659b99acee964a081376e32b2c68eee21185ca
kurosawa2357 <kurosawa23571113@gmail.com>
2023-08-07 18:04:24 +0900

    league

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(TeamSeeder::class);
+        $this->call(LeagueSeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
