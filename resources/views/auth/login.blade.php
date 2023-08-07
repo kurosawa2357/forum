f0dda23dc0b9b8274037783a6062fd679106e188
kurosawa2357 <kurosawa23571113@gmail.com>
2023-08-07 20:33:43 +0900

    delete googlelogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,11 +33,11 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <div class="flex items-center justify-end mt-4">
+            {{--<div class="flex items-center justify-end mt-4">
                 <a href="{{ url('auth/google') }}">
                     <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 0.01em;">
                 </a>
-            </div>
+            </div>--}}
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
